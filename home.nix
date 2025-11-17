f535613365247dea0148bfa7efac261a95186e0a
Don Williams <don.e.williams@gmail.com>
2025-11-16 19:55:03 -0500

    enable GTK dark theme

--- home.nix
+++ home.nix
@@ -5,38 +5,57 @@
   home.homeDirectory = "/home/dwilliams";
   home.stateVersion = "25.11";
   programs = {
-     neovim = {
-        enable = true;
-        defaultEditor = true;
-        };
-     bash = {
-       enable = true;
-       shellAliases = {
-         ll = "eza -la --group-dirs-first --icons";
-         v = "nvim";
-         rebuild = "sudo nixos-rebuild switch --flake ~/tony-nixos/";
-         update  = "nix flake update --flake ~/tony-nixos && sudo nixos-rebuild switch --flake ~/tony-nixos/";
+    neovim = {
+      enable = true;
+      defaultEditor = true;
     };
-    # The block below is for commands that should run every time a terminal starts.
-    initExtra = ''
-      # Source the personal file for all interactive shell sessions
-      if [ -f ~/.bashrc-personal ]; then
-       source ~/.bashrc-personal
-      fi
-    '';
-    profileExtra = ''
-      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-        #exec uwsm start -S hyprland-uwsm.desktop
-        exec Hyprland
-      fi
-    '';
+    bash = {
+      enable = true;
+      shellAliases = {
+        ll = "eza -la --group-dirs-first --icons";
+        v = "nvim";
+        rebuild = "sudo nixos-rebuild switch --flake ~/tony-nixos/";
+        update = "nix flake update --flake ~/tony-nixos && sudo nixos-rebuild switch --flake ~/tony-nixos/";
+      };
+      # The block below is for commands that should run every time a terminal starts.
+      initExtra = ''
+        # Source the personal file for all interactive shell sessions
+        if [ -f ~/.bashrc-personal ]; then
+         source ~/.bashrc-personal
+        fi
+      '';
+      profileExtra = ''
+        if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+          #exec uwsm start -S hyprland-uwsm.desktop
+          exec Hyprland
+        fi
+      '';
+    };
   };
- };
-    home.file.".config/hypr".source = ./config/hypr;
-    home.file.".config/waybar".source = ./config/waybar;
-    home.file.".config/fastfetch".source = ./config/fastfetch;
-    home.file.".config/kitty".source = ./config/kitty;
-    home.file.".config/foot".source = ./config/foot;
-    home.file.".bashrc-personal".source = ./config/.bashrc-personal;
-    home.file.".config/starship.toml".source = ./config/starship.toml;
+
+  gtk = {
+    enable = true;
+
+    gtk3.extraConfig = {
+      "gtk-application-prefer-dark-theme" = 1;
+    };
+
+    gtk4.extraConfig = {
+      "gtk-application-prefer-dark-theme" = 1;
+    };
+
+    # Optional: also explicitly pick a dark theme
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+  };
+
+  home.file.".config/hypr".source = ./config/hypr;
+  home.file.".config/waybar".source = ./config/waybar;
+  home.file.".config/fastfetch".source = ./config/fastfetch;
+  home.file.".config/kitty".source = ./config/kitty;
+  home.file.".config/foot".source = ./config/foot;
+  home.file.".bashrc-personal".source = ./config/.bashrc-personal;
+  home.file.".config/starship.toml".source = ./config/starship.toml;
 }
